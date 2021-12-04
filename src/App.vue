4a732fc8b34d00c262f294cb7ebfd1e27036b69f
Imran <44378535+Imranh21@users.noreply.github.com>
2021-12-04 16:56:52 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-screen max-h-screen flex flex-col">
+  <div class="w-full h-screen min-h-screen flex flex-col">
     
     <div class=" w-full flex flex-col relative bg-hero-pattern bg-cover justify-center items-center">
       <div class="w-4/12 py-10 pb-20 m-auto justify-center items-center text-center sm:w-10/12">
